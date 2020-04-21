5f0e6440fe08a777fbd2a87759784b4a839f7bd9
Diego Vernan <diegovernan@gmail.com>
2020-04-21 18:05:02 -0300

    Ajuste no charset

--- src/Config.php
+++ src/Config.php
@@ -6,6 +6,7 @@ class Config {
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
+    const DB_CHARSET = 'utf8mb4';
     const DB_DATABASE = 'mvc-2020';
     const DB_USER = 'root';
     const DB_PASS = 'root';
